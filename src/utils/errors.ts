7cb2836a3be9a8a9e1387462e78e2c09733c75ed
Addminus <adrianweniger@gmail.com>
2022-08-30 10:23:42 +0200

    fix: remove error code duplicates

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -14,14 +14,14 @@ export enum LifiErrorCode {
   ValidationError = 1001,
   TransactionUnderpriced = 1002,
   TransactionFailed = 1003,
-  TransactionUnprepared = 1008,
   Timeout = 1004,
   ProviderUnavailable = 1005,
   NotFound = 1006,
   ChainSwitchError = 1007,
-  SlippageNotMet = 1008,
-  SlippageError = 1008,
+  TransactionUnprepared = 1008,
   GasLimitError = 1009,
+  SlippageNotMet = 1010,
+  SlippageError = 1011,
 }
 
 export enum MetaMaskRPCErrorCode {
