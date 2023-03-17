0e24d7decc84e98e3a162ef44fb5ef0bff4c8e9f
abhikumar98 <abhishekkumar35962@gmail.com>
2023-03-17 17:18:38 +0530

    fix: invalid variable in Route object

--- src/helpers.ts
+++ src/helpers.ts
@@ -128,7 +128,6 @@ export const convertQuoteToRoute = (step: Step): Route => {
     toAmountUSD: step.estimate.toAmountUSD,
     steps: [lifiStep],
     toAmountMin: step.estimate.toAmountMin,
-    insurance: { state: 'NOT_INSURABLE', feeAmountUsd: '0' },
   }
 
   return route
