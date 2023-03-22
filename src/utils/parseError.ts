20613086ce38a4daa2b5043e6e730446a39d5e6c
abhikumar98 <abhishekkumar35962@gmail.com>
2023-03-22 19:51:24 +0700

    fix: remove consoles

--- src/utils/parseError.ts
+++ src/utils/parseError.ts
@@ -59,7 +59,6 @@ export const getTransactionNotSentMessage = async (
   step?: Step,
   process?: Process
 ): Promise<string> => {
-  console.trace('here')
   let transactionNotSend =
     'Transaction was not sent, your funds are still in your wallet'
 
