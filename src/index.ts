d573a1ef7ddcf8698a0f56e30be990fd056d85d8
Eugene Chybisov <imchybisov@gmail.com>
2024-05-20 10:13:50 +0200

    fix: export get/set allowance functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from '@lifi/types'
 export { config } from './config.js'
 export { EVM } from './core/EVM/EVM.js'
+export { getTokenAllowance } from './core/EVM/getAllowance.js'
+export { setTokenAllowance } from './core/EVM/setAllowance.js'
 export type {
   MultisigConfig,
   MultisigTransaction,
