4b03fb028c0626e4067f741ea6d71afef3135ada
Eugene Chybisov <imchybisov@gmail.com>
2025-01-14 13:53:35 +0100

    chore: adjust typeguard

--- src/core/EVM/typeguards.ts
+++ src/core/EVM/typeguards.ts
@@ -3,5 +3,5 @@ import type { EVMPermitStep } from './types.js'
 
 export function isEVMPermitStep(step: LiFiStepExtended): step is EVMPermitStep {
   const evmStep = step as EVMPermitStep
-  return 'permit' in evmStep || 'permitData' in evmStep || 'witness' in evmStep
+  return 'permit' in evmStep && 'permitData' in evmStep && 'witness' in evmStep
 }
