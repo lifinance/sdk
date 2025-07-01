e15bddb6ef98105e266b6dde047fa6acf239434b
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2025-07-01 13:30:28 +0200

    fix: adjust approval flow to allow native ERC20 tokens (#278)

--- src/utils/isZeroAddress.ts
+++ src/utils/isZeroAddress.ts
@@ -6,15 +6,3 @@ export const isZeroAddress = (address: string): boolean => {
   }
   return false
 }
-
-export const isNativeTokenAddress = (address: string): boolean => {
-  if (
-    address === AddressZero ||
-    address === AlternativeAddressZero ||
-    // CELO native token
-    address === '0x471ece3750da237f93b8e339c536989b8978a438'
-  ) {
-    return true
-  }
-  return false
-}
