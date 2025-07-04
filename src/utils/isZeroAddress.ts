ad352d274e80547371cbc2acaf529267c47b7614
Eugene Chybisov <imchybisov@gmail.com>
2025-07-04 11:28:27 +0200

    fix: adjust approval flow for native ERC20 tokens

--- src/utils/isZeroAddress.ts
+++ src/utils/isZeroAddress.ts
@@ -6,3 +6,17 @@ export const isZeroAddress = (address: string): boolean => {
   }
   return false
 }
+
+export const isNativeTokenAddress = (address: string): boolean => {
+  if (
+    address === AddressZero ||
+    address === AlternativeAddressZero ||
+    // Celo native token
+    address === '0x471ece3750da237f93b8e339c536989b8978a438' ||
+    // Metis native token
+    address === '0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000'
+  ) {
+    return true
+  }
+  return false
+}
