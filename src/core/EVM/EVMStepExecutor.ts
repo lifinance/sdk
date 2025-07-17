829e2d2c7c18403e9dd8cc6634823d50b93027c1
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2025-07-17 11:14:10 +0200

    fix: validate txHash format before generating transaction link (#283)

--- src/core/EVM/EVMStepExecutor.ts
+++ src/core/EVM/EVMStepExecutor.ts
@@ -15,7 +15,7 @@ import {
   sendTransaction,
   signTypedData,
 } from 'viem/actions'
-import { getAction } from 'viem/utils'
+import { getAction, isHex } from 'viem/utils'
 import { config } from '../../config.js'
 import { LiFiErrorCode } from '../../errors/constants.js'
 import { TransactionError } from '../../errors/errors.js'
@@ -650,17 +650,22 @@ export class EVMStepExecutor extends BaseStepExecutor {
         } as SendTransactionParameters)
       }
 
+      // Sometimes txHash can represent a batch hash or taskId rather than an individual transaction hash
+      // Those hashes might not be valid hex strings and should not be used to generate a txLink
+      const validatedTxHash = isHex(txHash, { strict: true })
+        ? txHash
+        : undefined
       process = this.statusManager.updateProcess(
         step,
         process.type,
         'PENDING',
         // When atomic batch or relayer are supported, txHash represents the batch hash or taskId rather than an individual transaction hash
         {
-          txHash,
           txType,
+          txHash: validatedTxHash,
           txLink:
-            txType === 'standard'
-              ? `${fromChain.metamask.blockExplorerUrls[0]}tx/${txHash}`
+            txType === 'standard' && validatedTxHash
+              ? `${fromChain.metamask.blockExplorerUrls[0]}tx/${validatedTxHash}`
               : undefined,
         }
       )
