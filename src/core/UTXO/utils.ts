3e693948a33d147fe7bc2242ca800eef1867ff86
tomide oladipo <ayotomiiide@gmail.com>
2025-08-05 11:48:55 +0100

    chore(bitcon): add comments to explain taproot support code

--- src/core/UTXO/utils.ts
+++ src/core/UTXO/utils.ts
@@ -9,6 +9,6 @@ export function isPsbtFinalized(psbt: Psbt): boolean {
   }
 }
 
-// helper function to convert full public key (33 bytes) to x-only format (32 bytes) as required by BIP 341
+// helper function to convert full public key (33 bytes) to x-only compressed format (32 bytes) required after taproot update
 export const toXOnly = (pubKey: Uint8Array) =>
   pubKey.length === 32 ? pubKey : pubKey.subarray(1, 33)
