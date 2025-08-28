aeb7d2a4bb32cc22ba181dbe91588af008c5b7e6
Tomide Oladipo <ayotomiiide@gmail.com>
2025-08-28 09:02:48 +0100

    refactor(utxo): make bigmi chain id dynamic (#296)
    Co-authored-by: Eugene Chybisov <18644653+chybisov@users.noreply.github.com>

--- src/core/UTXO/utils.ts
+++ src/core/UTXO/utils.ts
@@ -1,3 +1,5 @@
+import { ChainId as BigmiChainId } from '@bigmi/core'
+import { ChainId } from '@lifi/types'
 import type { Psbt } from 'bitcoinjs-lib'
 
 export function isPsbtFinalized(psbt: Psbt): boolean {
@@ -12,3 +14,12 @@ export function isPsbtFinalized(psbt: Psbt): boolean {
 // helper function to convert full public key (33 bytes) to x-only compressed format (32 bytes) required after taproot update
 export const toXOnly = (pubKey: Uint8Array) =>
   pubKey.length === 32 ? pubKey : pubKey.subarray(1, 33)
+
+export const toBigmiChainId = (chainId: ChainId): BigmiChainId => {
+  switch (chainId) {
+    case ChainId.BTC:
+      return BigmiChainId.BITCOIN_MAINNET
+    default:
+      throw new Error(`Unsupported chainId mapping: ${chainId}`)
+  }
+}
