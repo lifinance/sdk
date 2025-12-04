ffd4e0021fcc84e1f88172a4b203ebaf2a723e02
Lizaveta Miasayedava <effie.mias@gmail.com>
2025-12-04 11:19:50 +0000

    fix: use cached chains in getMulticallAddress

--- packages/sdk-provider-ethereum/src/actions/getMulticallAddress.ts
+++ packages/sdk-provider-ethereum/src/actions/getMulticallAddress.ts
@@ -1,13 +1,12 @@
-import { type ChainId, ChainType, getChains, type SDKClient } from '@lifi/sdk'
+import { type ChainId, ChainType, type SDKClient } from '@lifi/sdk'
 import type { Address } from 'viem'
 
 export const getMulticallAddress = async (
   client: SDKClient,
   chainId: ChainId
 ): Promise<Address | undefined> => {
-  const chains = await getChains(client, {
-    chainTypes: [ChainType.EVM],
-  })
-  return chains?.find((chain) => chain.id === chainId)
-    ?.multicallAddress as Address
+  const chains = await client.getChains()
+  return chains?.find(
+    (chain) => chain.id === chainId && chain.chainType === ChainType.EVM
+  )?.multicallAddress as Address
 }
