a3f1ab9c2148dfdcba8aba2eed5e708ab4fee2ed
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2026-01-12 14:15:43 +0100

    fix: update maxPriorityFeePerGas calculation and client check (#342)

--- src/core/EVM/EVMStepExecutor.ts
+++ src/core/EVM/EVMStepExecutor.ts
@@ -229,8 +229,8 @@ export class EVMStepExecutor extends BaseStepExecutor {
   }
 
   private prepareUpdatedStep = async (
-    client: Client,
     step: LiFiStepExtended,
+    process: Process,
     signedTypedData?: SignedTypedData[]
   ) => {
     // biome-ignore lint/correctness/noUnusedVariables: destructuring
@@ -375,6 +375,20 @@ export class EVMStepExecutor extends BaseStepExecutor {
 
     let transactionRequest: TransactionParameters | undefined
     if (step.transactionRequest) {
+      // Only call checkClient for local accounts when we need to get maxPriorityFeePerGas
+      let maxPriorityFeePerGas: bigint | undefined
+      if (this.client.account?.type === 'local') {
+        const updatedClient = await this.checkClient(step, process)
+        if (!updatedClient) {
+          return null
+        }
+        maxPriorityFeePerGas = await getMaxPriorityFeePerGas(updatedClient)
+      } else {
+        maxPriorityFeePerGas = step.transactionRequest.maxPriorityFeePerGas
+          ? BigInt(step.transactionRequest.maxPriorityFeePerGas)
+          : undefined
+      }
+
       transactionRequest = {
         chainId: step.transactionRequest.chainId,
         to: step.transactionRequest.to,
@@ -392,12 +406,7 @@ export class EVMStepExecutor extends BaseStepExecutor {
         // maxFeePerGas: step.transactionRequest.maxFeePerGas
         //   ? BigInt(step.transactionRequest.maxFeePerGas as string)
         //   : undefined,
-        maxPriorityFeePerGas:
-          client.account?.type === 'local'
-            ? await getMaxPriorityFeePerGas(client)
-            : step.transactionRequest.maxPriorityFeePerGas
-              ? BigInt(step.transactionRequest.maxPriorityFeePerGas)
-              : undefined,
+        maxPriorityFeePerGas,
       }
     }
 
@@ -619,15 +628,16 @@ export class EVMStepExecutor extends BaseStepExecutor {
 
       await checkBalance(this.client.account!.address, step)
 
-      // Make sure that the client and chain is still correct
-      const updatedClient = await this.checkClient(step, process)
-      if (!updatedClient) {
+      // Try to prepare a new transaction request and update the step with typed data
+      const preparedStep = await this.prepareUpdatedStep(
+        step,
+        process,
+        signedTypedData
+      )
+      if (!preparedStep) {
         return step
       }
-
-      // Try to prepare a new transaction request and update the step with typed data
-      let { transactionRequest, isRelayerTransaction } =
-        await this.prepareUpdatedStep(updatedClient, step, signedTypedData)
+      let { transactionRequest, isRelayerTransaction } = preparedStep
 
       process = this.statusManager.updateProcess(
         step,
